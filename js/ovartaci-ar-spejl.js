68bd9c5ecebdc4402272e5dffff96d7e3b0421d9
benedictefrandsen <benedicte.frandsen@gmail.com>
2026-05-27 11:40:01 +0200

    Tilføjelse af identiter
    Dragen, Tigeren og Ovartaci

--- js/ovartaci-ar-spejl.js
+++ js/ovartaci-ar-spejl.js
@@ -26,7 +26,31 @@ const identiteter = [
     Navn: "Fuglen i paradis",
     Kunster: "Ovartaci",
     Årstal:
-    Beskrivelse:"“Jeg begyndte som en paradisfugl, og under min flugt blev jeg til sidst forvandlet til en måge og ankom til Grønland, men det er mange år siden.” 
+    Beskrivelse:"Jeg begyndte som en paradisfugl, og under min flugt blev jeg til sidst forvandlet til en måge og ankom til Grønland, men det er mange år siden.” 
 
+  },
+
+  {
+    id: 4,
+    Navn: "Dragen",
+    Kunster: "Ovartaci",
+    Årstal:
+    Beskrivelse: "Drager i Ovartacis kosmologi fungerede ikke som monstre, men som spirituelle symboler på lykke, værdighed, transformation og personlig frihed.  Personlig frihed: En flugtvej og et sindbillede på at kunne bryde ud af psykiatriens fysiske og mentale rammer. Værdighed og styrke: Dragen stod for stolthed og livskvalitet. Transformation: En sjælelig forvandling, der ofte blev kædet sammen med genfødsel og kvindens psyke.""
+  },
+
+  {
+    id: 5,
+    Navn: "Tigeren",
+    Kunster: "Ovartaci",
+    Årstal:
+    Beskrivelse: "'Det er et menneske i tigerskikkelse eller en tiger, der rejser sig på bagbenene, men se, den har jo tæer som mennesket og ikke som en tiger. Det er et menneske i tigerskikkelse. Jeg var selv tiger og blev forvandlet til menneske'. Sådan fortæller Ovartaci i samtalebogen 'Ovartaci - Pictures, Thoughts and Visions of an Artist'. Netop denne flydende tilstand mellem dyr og mennesker er kendetegnende for Ovartaci."
+  },
+
+{
+    id: 6,
+    Navn: "Ovartaci",
+    Kunster: "Ovartaci",
+    Årstal:
+    Beskrivelse: "Ovartaci blev født som Louis Marcussen og voksede op i en fattig familie i Jylland. Som ung mand arbejdede han blandt andet som malersvend og rejste også i en periode til Argentina, hvor han arbejdede under hårde forhold. Rejserne og oplevelserne gjorde stort indtryk på ham og blev senere en del af hans fantasiverden i kunsten. I sine yngre år levede han udadtil som mand og blev beskrevet som intelligent, kreativ og meget fantasifuld. Senere i livet ændrede hans forhold til køn og identitet sig markant. Han ønskede i lange perioder at leve som kvinde og skabte mange kvindelige figurer og selvfremstillinger i kunsten."
   },
 ];
